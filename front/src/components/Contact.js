068da23832853d3265f177d20c7ab9c393fda940
Charly <charly.flu.67@gmail.com>
2023-09-22 12:14:28 +0200

    Updated linkedin link

--- front/src/components/Contact.js
+++ front/src/components/Contact.js
@@ -11,7 +11,7 @@ function Contact ({t}){
             <div className='contact__container'>
                 <Link className='contact__item__link' to='https://www.linkedin.com/in/charly-flu-3004b0271/' target="_blank">
                     <i className="fa-brands fa-linkedin"></i>
-                    in/charly-flu-3004b0271/
+                    in/charlyflu/
                 </Link>
                 <Link className='contact__item__link' to='mailto: charly.flu.67.150@gmail.com' target="_blank">
                     <i className="fa-solid fa-envelope"></i>
