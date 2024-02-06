b3f7a87222fc9683301a23e16e71b81e9336aab1
Charly <charly.flu.67@gmail.com>
2024-02-06 19:29:38 +0100

    Fixed weird bug in a weird way

--- front/src/components/Rain.jsx
+++ front/src/components/Rain.jsx
@@ -1,15 +1,15 @@
 import './Rain.css';
-import React from 'react';
-import { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function Rain() {
     const [isRaining, setisRaining] = useState(false);
-    let needSnow = false;
-    let needLeaf = false;
-    let needSakura = false;
 
-    const letItRain = () => {
-        const container = document.getElementById('rain-container');
+    const[sakuring, setSakuring] = useState(false);
+    const[leafing, setLeafing] = useState(false);
+    const[snowing, setSnowing] = useState(false);
+
+    const letItRain = (needSnow, needLeaf, needSakura) => {
+        const container = document.getElementById('rain__container__id');
 
         if (isRaining) {
             container.innerHTML = '';
@@ -19,38 +19,40 @@ function Rain() {
 
         setisRaining(true); // Set flag to true
 
-        const snowflakeCount = 50;
+        const rainObjectCount = 50;
 
-        for (let i = 0; i < snowflakeCount; i++) {
-            const snowflake = document.createElement('div');
-            snowflake.className = needSnow ? 'snowflake' : 'leaf';
+        for (let i = 0; i < rainObjectCount; i++) {
+            const rainObject = document.createElement('div');
+            rainObject.className = needSnow ? 'snowflake' : 'leaf';
 
-            const xPos = Math.random() * window.innerWidth;
-            snowflake.style.left = xPos + 'px';
+            const xPos = Math.random() * 100;
+            if (container) {
+                rainObject.style.left = xPos + '%';
 
-            snowflake.style.top = -50 + 'px';
+                rainObject.style.top = -50 + 'px';
 
-            const size = Math.random() * 15 + 5;
-            snowflake.style.width = size + 'px';
-            snowflake.style.height = size + 'px';
+                const size = Math.random() * 15 + 5;
+                rainObject.style.width = size + 'px';
+                rainObject.style.height = size + 'px';
 
-            const speed = Math.random() * 5 + 3; // Slower speed between 1px/s and 2.5px/s
-            snowflake.style.animationDuration = speed + 's';
+                const speed = Math.random() * 5 + 3; // Slower speed between 1px/s and 2.5px/s
+                rainObject.style.animationDuration = speed + 's';
 
-            const sway = Math.random() * 10 - 5; // Random sway between -5px and 5px
-            snowflake.style.transform = `translateY(0px) translateX(${sway}px)`; // Added horizontal movement
+                const sway = Math.random() * 10 - 5; // Random sway between -5px and 5px
+                rainObject.style.transform = `translateY(0px) translateX(${sway}px)`; // Added horizontal movement
+            }
 
             const opacity = Math.random() * 0.5 + 0.3; // Random opacity between 0.7 and 1
-            snowflake.style.opacity = opacity;
+            rainObject.style.opacity = opacity;
 
             const delay = Math.random() * 7 // Random delay between 0s and 3s
-            snowflake.style.animationDelay = delay + 's'; // Set animation delay
+            rainObject.style.animationDelay = delay + 's'; // Set animation delay
 
-            container.appendChild(snowflake);
+            container.appendChild(rainObject);
 
-            
+
             if (i % 3 === 0) {
-                snowflake.classList.add('blurry');
+                rainObject.classList.add('blurry');
             }
 
             let classNameAnim = "rain_anim1";
@@ -60,81 +62,72 @@ function Rain() {
             else if (i % 3 === 2) {
                 classNameAnim = "rain_anim3";
             }
+            if(needSnow){
+                setSnowing(true);
+            }
 
             if (needLeaf) {
                 //set image of leaf
                 for (let y = 0; y < 7; y++) {
                     if (i % 7 === y) {
-                        snowflake.style.backgroundImage = "url(" + process.env.PUBLIC_URL + "/images/rains/leaf" + (y + 1) + ".png)";
+                        rainObject.style.backgroundImage = "url(" + process.env.PUBLIC_URL + "/images/rains/leaf" + (y + 1) + ".png)";
                     }
                 }
                 //bigger size
-                snowflake.style.width = 50 + 'px';
-                snowflake.style.height = 50 + 'px';
+                rainObject.style.width = 50 + 'px';
+                rainObject.style.height = 50 + 'px';
+                setLeafing(true);
             }
-            else if(needSakura){
-                //set image of leaf
+            else if (needSakura) {
+                //set image of sakura
                 for (let y = 0; y < 7; y++) {
                     if (i % 7 === y) {
-                        snowflake.style.backgroundImage = "url(" + process.env.PUBLIC_URL + "/images/rains/sakura" + (y + 1) + ".png)";
+                        rainObject.style.backgroundImage = "url(" + process.env.PUBLIC_URL + "/images/rains/sakura" + (y + 1) + ".png)";
                     }
                 }
                 //bigger size
-                snowflake.style.width = 50 + 'px';
-                snowflake.style.height = 50 + 'px';
+                rainObject.style.width = 50 + 'px';
+                rainObject.style.height = 50 + 'px';
+                setSakuring(true);
             }
 
-            snowflake.classList.add(classNameAnim);
+            rainObject.classList.add(classNameAnim);
 
         }
     }
     const d = new Date();
     const month = d.getMonth();
 
-
-
-    //if it's december or january, let it snow
-    if (month === 11 || month === 0 || month === 1) {
-        needSnow = true;
-        //wait the page to be loaded
-        window.addEventListener('load', function () {
-            letItRain();
-        });
-    }
-    //if it's september, october or november, let it leaf ?
-    if (month === 8 || month === 9 || month === 10) {
-        needLeaf = true;
-        //wait the page to be loaded
-        window.addEventListener('load', function () {
-            letItRain();
-        });
-    }
-    //if it's march, april or may, let it sakura ?
-    if (month === 2 || month === 3 || month === 4) {
-        needSakura = true;
-        //wait the page to be loaded
-        window.addEventListener('load', function () {
-            letItRain();
-        });
-    }
-    
-
-
+    useEffect(() => {
+        //if it's december or january, let it snow
+        if (month === 11 || month === 0 || month === 1) {
+            letItRain(true, false, false);
+        }
+        //if it's september, october or november, let it leaf ?
+        if (month === 8 || month === 9 || month === 10) {
+            letItRain(false, true, false);
+        }
+        //if it's march, april or may, let it sakura ?
+        if (month === 2 || month === 3 || month === 4) {
+            letItRain(false, false, true);
+        }
+        // eslint-disable-next-line
+    }, []);
 
     return (
         <>
-            <div className="rain-container" id="rain-container">
+            <div className="rain-container" id="rain__container__id">
 
             </div>
-            {needSnow && <button className={isRaining ? "rain-button active" : "rain-button"} onClick={letItRain}>
+            {snowing && <button className={isRaining ? "rain-button active" : "rain-button"} onClick={letItRain}>
                 <img src={process.env.PUBLIC_URL + "/images/rains/snow_flake.png"} alt="" />
             </button>
             }
-            {needLeaf && <button className={isRaining ? "rain-button active" : "rain-button"} onClick={letItRain}>
+            {leafing && <button className={isRaining ? "rain-button active" : "rain-button"} onClick={letItRain}>
                 <img src={process.env.PUBLIC_URL + "/images/rains/leaf1.png"} alt="" />
             </button>
             }
-            {needSakura && <button className={isRaining ? "rain-button active" : "rain-button"} onClick={letItRain}>
+            {sakuring && <button className={isRaining ? "rain-button active" : "rain-button"} onClick={letItRain}>
                 <img src={process.env.PUBLIC_URL + "/images/rains/sakura_big.png"} alt="" />
             </button>
             }
