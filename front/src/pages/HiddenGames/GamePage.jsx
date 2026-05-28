7e0a55b50c4e3b98b7555ae04a8e4b8e540fb688
Charly <charly.flu.67@gmail.com>
2026-05-28 12:16:36 +0200

    feat: better accessibility for small devices

--- front/src/pages/HiddenGames/GamePage.jsx
+++ front/src/pages/HiddenGames/GamePage.jsx
@@ -1,6 +1,5 @@
 import { Link, useParams } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
-import Rain from '../../components/effects/Rain';
 import SlideInOnScroll from '../../components/ui/SlideInOnScroll';
 import { findGame } from '../../games/games';
 import './HiddenGames.css';
@@ -12,28 +11,6 @@ function GamePage() {
 
   if (!game) {
     return (
-      <>
-        <Rain />
-        <div className='hidden-games'>
-          <div className='hidden-games__topbar'>
-            <Link to='/hidden-games' className='games-back-button'>
-              <i className='fa-solid fa-arrow-left' aria-hidden='true'></i>
-              <span>{t('games_back_hub')}</span>
-            </Link>
-          </div>
-          <SlideInOnScroll>
-            <h1 className='hidden-games__title'>{t('games_not_found')}</h1>
-          </SlideInOnScroll>
-        </div>
-      </>
-    );
-  }
-
-  const GameComponent = game.component;
-
-  return (
-    <>
-      <Rain />
       <div className='hidden-games'>
         <div className='hidden-games__topbar'>
           <Link to='/hidden-games' className='games-back-button'>
@@ -41,18 +18,34 @@ function GamePage() {
             <span>{t('games_back_hub')}</span>
           </Link>
         </div>
-
         <SlideInOnScroll>
-          <h1 className='hidden-games__title'>{t(game.titleKey)}</h1>
-        </SlideInOnScroll>
-
-        <SlideInOnScroll>
-          <main className='hidden-games__game-host'>
-            <GameComponent />
-          </main>
+          <h1 className='hidden-games__title'>{t('games_not_found')}</h1>
         </SlideInOnScroll>
       </div>
-    </>
+    );
+  }
+
+  const GameComponent = game.component;
+
+  return (
+    <div className='hidden-games'>
+      <div className='hidden-games__topbar'>
+        <Link to='/hidden-games' className='games-back-button'>
+          <i className='fa-solid fa-arrow-left' aria-hidden='true'></i>
+          <span>{t('games_back_hub')}</span>
+        </Link>
+      </div>
+
+      <SlideInOnScroll>
+        <h1 className='hidden-games__title'>{t(game.titleKey)}</h1>
+      </SlideInOnScroll>
+
+      <SlideInOnScroll>
+        <main className='hidden-games__game-host'>
+          <GameComponent />
+        </main>
+      </SlideInOnScroll>
+    </div>
   );
 }
 
--- front/src/pages/HiddenGames/HiddenGames.jsx
+++ front/src/pages/HiddenGames/HiddenGames.jsx
@@ -1,6 +1,5 @@
 import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
-import Rain from '../../components/effects/Rain';
 import SlideInOnScroll from '../../components/ui/SlideInOnScroll';
 import { games } from '../../games/games';
 import './HiddenGames.css';
@@ -9,38 +8,35 @@ function HiddenGames() {
   const { t } = useTranslation();
 
   return (
-    <>
-      <Rain />
-      <div className='hidden-games'>
-        <div className='hidden-games__topbar'>
-          <Link to='/' className='games-back-button'>
-            <i className='fa-solid fa-arrow-left' aria-hidden='true'></i>
-            <span>{t('games_back_home')}</span>
-          </Link>
-        </div>
-
-        <SlideInOnScroll>
-          <h1 className='hidden-games__title'>{t('games_hub_title')}</h1>
-          <p className='hidden-games__subtitle'>{t('games_hub_subtitle')}</p>
-        </SlideInOnScroll>
-
-        <SlideInOnScroll>
-          <ul className='hidden-games__list'>
-            {games.map((game) => (
-              <li key={game.id} className='hidden-games__card'>
-                <Link to={`/hidden-games/${game.id}`} className='hidden-games__card-link'>
-                  <h2 className='hidden-games__card-title'>{t(game.titleKey)}</h2>
-                  <p className='hidden-games__card-desc'>{t(game.descriptionKey)}</p>
-                  {!game.available && (
-                    <span className='hidden-games__badge'>{t('games_coming_soon')}</span>
-                  )}
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </SlideInOnScroll>
+    <div className='hidden-games'>
+      <div className='hidden-games__topbar'>
+        <Link to='/' className='games-back-button'>
+          <i className='fa-solid fa-arrow-left' aria-hidden='true'></i>
+          <span>{t('games_back_home')}</span>
+        </Link>
       </div>
-    </>
+
+      <SlideInOnScroll>
+        <h1 className='hidden-games__title'>{t('games_hub_title')}</h1>
+        <p className='hidden-games__subtitle'>{t('games_hub_subtitle')}</p>
+      </SlideInOnScroll>
+
+      <SlideInOnScroll>
+        <ul className='hidden-games__list'>
+          {games.map((game) => (
+            <li key={game.id} className='hidden-games__card'>
+              <Link to={`/hidden-games/${game.id}`} className='hidden-games__card-link'>
+                <h2 className='hidden-games__card-title'>{t(game.titleKey)}</h2>
+                <p className='hidden-games__card-desc'>{t(game.descriptionKey)}</p>
+                {!game.available && (
+                  <span className='hidden-games__badge'>{t('games_coming_soon')}</span>
+                )}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </SlideInOnScroll>
+    </div>
   );
 }
 
